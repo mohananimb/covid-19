557ff24745b30b604bdb9fa60466bb52399c41e3
abc <abc@example.com>
2020-04-16 22:13:17 +0530

    fi

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { fontSize } from '@material-ui/system'
+
+export default function Footer() {
+    return (
+        <footer style={{marginTop: 80 + "px", fontSize: 20 + "px"}}>
+        <p>Created by: Manish Mohanani</p>
+      </footer>
+    )
+}
